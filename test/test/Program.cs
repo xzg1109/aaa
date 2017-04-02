98a47af8b48e6e554beaa3f97538aab2f88f769c
xzg1109 <xzg1109@126.com>
2017-04-03 00:58:42 +0800

    first commit

---
+++ test/test/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("111111111111");
+            Console.ReadLine();
+
+        }
+    }
+}
